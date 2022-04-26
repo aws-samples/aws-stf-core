b83445ae37db7347fb9ff5eb05c047ad13238889
Ali Benfattoum <alidiali@gmail.com>
2022-04-26 10:02:41 +0200

    update link to ECR Scorpio image

--- parameters.ts
+++ parameters.ts
@@ -1,5 +1,5 @@
 export const Parameters = {
-    image_context_broker: 'public.ecr.aws/j9u9o5d1/scorpio-aio:latest', 
+    image_context_broker: 'public.ecr.aws/scorpiobroker/scorpio-aio:latest', 
     smart_data_model_url : 'https://raw.githubusercontent.com/smart-data-models/data-models/master/context.jsonld',
     shadow_prefix: "Stf",
     timeout: '0', // Timeout for the API call in the Lambda that sync with context broker. Has to be a string to pass it in env variable  
